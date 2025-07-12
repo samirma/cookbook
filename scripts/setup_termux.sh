d96464ac6d091d86aba0449ae6a8b054ceb2cce4
Samir Moreira Antonio <samir.ma@gmail.com>
2025-07-12 20:42:27 +0100

    Update setup_termux.sh

--- scripts/setup_termux.sh
+++ scripts/setup_termux.sh
@@ -6,7 +6,7 @@ pkg update -y
 
 # Install vim, wget, git, openssh, and iproute2
 echo "Installing vim, wget, git, openssh, and iproute2..."
-pkg install vim wget git openssh iproute2  cmake ccache libzmq  rsync  avahi root-repo -y
+pkg install vim wget git openssh iproute2  cmake ccache libzmq  rsync  avahi root-repo  file -y
 
 echo "OpenCL section"
 pkg install clinfo ocl-icd opencl-headers fastfetch -y
