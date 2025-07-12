09c9e088b4cf3980d2867abf7df4c532e6fc3cdc
Samir Moreira Antonio <samir.ma@gmail.com>
2025-07-13 00:52:41 +0100

    Update publish_worker.sh

--- scripts/publish_worker.sh
+++ scripts/publish_worker.sh
@@ -4,16 +4,10 @@
 avahi-daemon &
 
 USER_NAME=$(whoami)
-IP_ADDRESS=$(ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' || echo "YOUR_DEVICE_IP")
 PORT=$(sshd -T | grep "port" | awk '{print $2}' | head -n 1)
 
-if [ "$IP_ADDRESS" = "YOUR_DEVICE_IP" ]; then
-    echo "Could not automatically determine IP address for wlan0."
-    echo "Please find it manually using 'ip addr' or 'ifconfig' and replace YOUR_DEVICE_IP."
-    exit 1
-fi
 
 while true; do
-  avahi-publish-service "Termux Worker ${USER_NAME}" _ssh._tcp $PORT "user=${USER_NAME}" "ip=${IP_ADDRESS}"
+  avahi-publish-service "Termux Worker ${USER_NAME}" _ssh._tcp $PORT "user=${USER_NAME}" "worker"
   sleep 10
 done
