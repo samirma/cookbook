e2f1eaf5c0cc772b980bf09f68cde91e85841870
Samir Moreira Antonio <samir.ma@gmail.com>
2025-07-14 00:39:55 +0100

    Update publish_worker.py

--- scripts/publish_worker.py
+++ scripts/publish_worker.py
@@ -4,17 +4,6 @@ import time
 import subprocess
 from zeroconf import ServiceInfo, Zeroconf
 
-def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except Exception:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
 
 def get_ssh_port():
     # This is a bit of a hack, but it's the most reliable way to get the SSH port in Termux
@@ -35,16 +24,13 @@ def get_username():
 def main():
     user_name = get_username()
     port = get_ssh_port()
-    ip_address = get_ip_address()
-    hostname = socket.gethostname()
 
     zeroconf = Zeroconf()
     service_info = ServiceInfo(
         "_ssh._tcp.local.",
         f"Termux Worker {user_name}._ssh._tcp.local.",
-        addresses=[socket.inet_aton(ip_address)],
         port=port,
-        properties={'user': user_name, 'hostname': hostname},
+        properties={'user': user_name},
         server=f"{hostname}.local.",
     )
     zeroconf.register_service(service_info)
