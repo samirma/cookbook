fd4d084d747ac110096730ec6dffdca3c07fe6e8
Samir Moreira Antonio <samir.ma@gmail.com>
2025-07-14 00:32:26 +0100

    Update discover_workers.sh

--- scripts/discover_workers.sh
+++ scripts/discover_workers.sh
@@ -3,12 +3,6 @@
 # This script will be run on the master server.
 # It will discover workers on the network and print the SSH command to connect to them.
 
-# Check if avahi-browse is installed
-if ! command -v avahi-browse &> /dev/null; then
-  echo "avahi-browse could not be found. Please install avahi-utils."
-  exit 1
-fi
-
 echo "Discovering workers..."
 avahi-browse -r -t _ssh._tcp | grep "Termux Worker" | while read line; do
   if [[ $line == "="* ]]; then
