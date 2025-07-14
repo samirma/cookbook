9c0edb2f4daf1e0596327eb6370c8638d5f7c28f
Samir Moreira Antonio <samir.ma@gmail.com>
2025-07-14 10:07:20 +0100

    Update discover_workers.sh

--- scripts/discover_workers.sh
+++ scripts/discover_workers.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-
 echo "Discovering workers..."
-# Use the parsable format and filter for "Termux Worker"
-avahi-browse -r -p -t _ssh._tcp | grep ";Termux Worker" | while IFS=';' read -r _ interface _ name _ _ host ip port txt; do
+# Use the parsable format and filter for the new service name "Termux-Worker"
+avahi-browse -r -p -t _ssh._tcp | grep ";Termux-Worker-" | while IFS=';' read -r _ interface _ name _ _ host ip port txt; do
   # Ensure the name field is not empty
   if [ -n "$name" ]; then
     # Use sed to reliably extract the user from the text field
