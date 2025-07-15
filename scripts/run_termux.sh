bcefb335b08f4c6522ba6ba5fe09f5abf0df23f2
Samir Moreira Antonio <samir.ma@gmail.com>
2025-07-15 19:54:45 +0100

    Update run_termux.sh

--- scripts/run_termux.sh
+++ scripts/run_termux.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# Function to print a formatted message
 print_message() {
   echo
   echo "--------------------------------------------------"
@@ -10,16 +13,19 @@ chmod +x ~/publish_worker.py
 nohup python ~/publish_worker.py &
 echo "Worker's SSH service is being published in the background."
 
+# Check if Termux storage is already set up
+if [ -z "$(ls -A ~/storage)" ]; then
+  print_message "Setting up Termux storage..."
+  termux-setup-storage
 
-print_message "Setting up Termux storage..."
-termux-setup-storage
-
-# Create a symlink for llama.cpp cache
-mkdir -p ~/storage/dcim/llama.cpp
-ln -sfn ~/storage/dcim/llama.cpp /data/data/com.termux/files/home/.cache/llama.cpp
+  # Create a symlink for llama.cpp cache
+  mkdir -p ~/storage/dcim/llama.cpp
+  ln -sfn ~/storage/dcim/llama.cpp /data/data/com.termux/files/home/.cache/llama.cpp
+else
+  print_message "Termux storage already set up."
+fi
 
 print_message "Starting SSH server..."
 sshd
 
 fastfetch
-
